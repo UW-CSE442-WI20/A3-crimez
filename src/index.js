8289d938f4bf5d078be171a2c5fa24dd6ad98484
Lior Levy <lior7212@gmail.com>
2020-02-04 16:24:08 -0800

    gradience based on count :)

--- src/index.js
+++ src/index.js
@@ -60,6 +60,10 @@ Promise.all([
 		  })
 		  .on("mouseover", function (d) {
 			  tooltip.style("visibility", "visible");
+		  })
+		  .on("mousemove", function (d) {
+			  tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px")
+				  .text(d.properties.BoroName);
 		  });
 	}).catch(function(err){
 			console.log(err)

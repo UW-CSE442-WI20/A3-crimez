d512dc0b6838c57c86e750b61c6b8df4c7ad95b5
hmharvey <hmharvey@github.com>
2020-02-04 17:14:53 -0800

    Add more hover functionality

--- src/index.js
+++ src/index.js
@@ -16,10 +16,11 @@ var path = d3.geoPath()
 var data = d3.map();
 var colorScale = d3.scaleThreshold()
 	.domain([10000, 25000, 50000, 75000, 100000])
-	.range(d3.schemeBlues[5]);
+	.range(d3.schemePurples[5]);
 
 var tooltip = d3.select("body")
 	.append("div")
+        .attr("class", "tooltip")
 	.style("position", "absolute")
 	.style("z-index", "10")
 	.style("visibility", "hidden");
@@ -33,7 +34,7 @@ Promise.all([
 		counts = {}
 		for(var i = 0; i < topo[1].length; i++){
 
-		   if (counts[topo[1][i]["BORO_NM"]]) {
+			if (counts[topo[1][i]["BORO_NM"]]) {
 			  counts[topo[1][i]["BORO_NM"]] += 1
 		   } else{
 			  counts[topo[1][i]["BORO_NM"]] = 1
@@ -62,111 +63,16 @@ Promise.all([
 			  tooltip.style("visibility", "visible");
 		  })
 		  .on("mousemove", function (d) {
-			  tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px")
-				  .text(d.properties.BoroName);
+			  d3.select(this).style("fill", "lightgrey");
+			  tooltip.style("top", (d3.event.pageY - 10) + "px")
+				 .style("left", (d3.event.pageX + 10) + "px")
+				 .text(d.properties.BoroName + ": " +  counts[d.properties.BoroName.toUpperCase()] + " crimes");
+		  })
+		  .on("mouseout", function (d) {
+			  d3.select(this).style("fill", colorScale(counts[d.properties.BoroName.toUpperCase()]));
+			  tooltip.style("visibility", "hidden")
 		  });
 	}).catch(function(err){
 			console.log(err)
 		}
 	) 
-
-
-// Load external data and boot
-// d3.queue()
-//   .defer(d3.json, "./data/boroughs.geojson")
-//   .defer(d3.json, "https://data.cityofnewyork.us/resource/5uac-w243.json")
-//   .await(ready);
-
-// function ready(error, topo, reg) {
-
-// 	console.log(reg);
-//   // Draw the map
-//   svg.append("g")
-//     .selectAll("path")
-//     .data(topo.features)
-//     .enter()
-//     .append("path")
-//       // draw each country
-//       .attr("d", d3.geoPath()
-//         .projection(projection)
-// 	  )
-
-//       // set the color of each country
-//       .attr("fill", function (d) {
-// 		  //TODO: group by borough and count number of reports 
-// 		  counts = {}
-// 		  for(var i = 0; i < reg.length; i++){
-
-// 			 if (counts[reg[i].boro_nm]) {
-// 				counts[reg[i].boro_nm] += 1
-// 			 } else{
-// 				counts[reg[i].boro_nm] = 1
-// 			 }
-// 		  }
-
-// 		  console.log(counts);
-//         // d.total = data.get(d.id) || 0;
-//         return colorScale(d.properties.BoroCode);
-//       });
-//     }
-
-//WHAT d RETURNS
-// type: "Feature"
-// id: 4
-// properties:
-// BoroCode: 2
-// BoroName: "Bronx"
-// Shape_Leng: 464517.89055
-// Shape_Area: 1186804144.79
-// __proto__: Object
-// geometry: {type: "MultiPolygon", coordinates: Array(24)}
-
-
-// svg = d3.select("body").append("svg")
-// 	.attr("width", width)
-// 	.attr("height", height);
-
-// var tooltip = d3.select("body")
-// 	.append("div")
-// 	.style("position", "absolute")
-// 	.style("z-index", "10")
-// 	.style("visibility", "hidden");
-
-// color = d3.scaleQuantize([1, 10], d3.schemeBlues[9])
-
-// d3.json("./data/boroughs.geojson", function (error, nyb) {
-// 	if (error) return console.log(error)
-// 	console.log(nyb)
-
-// 	var projection = d3.geo.mercator()
-// 		.center([-73.94, 40.70])
-// 		.scale(50000)
-// 		.translate([(width) / 2, (height) / 2]);
-
-// 	var path = d3.geo.path()
-// 		.projection(projection);
-
-// 	var g = svg.append("g").attr("id", "boroughs");
-
-// 	g.append("g")
-// 		.selectAll(".state")
-// 		.data(nyb.features)
-// 		.enter().append("path")
-// 		.attr("class", function (d) { return d.properties.BoroName; })
-// 		.attr("d", path)
-// 		.on("mouseover", function (d) {
-// 			console.log(d)
-// 			tooltip.style("visibility", "visible");
-// 		})
-// 		.on("mousemove", function (d) {
-// 			d3.select(this).style("fill", "orange")
-// 			tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px")
-// 				.text(d.properties.BoroName);
-// 		})
-// 		.on("mouseout", function () {
-// 			d3.select(this).style("fill", "steelblue")
-// 			tooltip.style("visibility", "hidden");
-
-// 		});
-
-// });
